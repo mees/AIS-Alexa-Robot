67eb06c418cfa6ca61b9da5b85fcc8178ad353e8
Oier Mees <oier.mees@gmail.com>
2021-02-02 19:02:13 +0100

    pre-commit

--- test_response.py
+++ test_response.py
@@ -2,33 +2,31 @@
 import rospy
 import std_msgs
 
-
-MESSAGE_SEPARATOR = '@@'
+MESSAGE_SEPARATOR = "@@"
 
 ROS_STATE = dict()
 
+
 def main():
-    rospy.init_node('auto_reply')
-    ROS_STATE['pub'] = rospy.Publisher('/alexa/reply', std_msgs.msg.String, queue_size=1)
-    ROS_STATE['sub'] = rospy.Subscriber('/alexa/command', std_msgs.msg.String, receive_command)
-    print('set up publisher and subscriber, spinning...')
+    rospy.init_node("auto_reply")
+    ROS_STATE["pub"] = rospy.Publisher("/alexa/reply", std_msgs.msg.String, queue_size=1)
+    ROS_STATE["sub"] = rospy.Subscriber("/alexa/command", std_msgs.msg.String, receive_command)
+    print("set up publisher and subscriber, spinning...")
     rospy.spin()
 
 
 def receive_command(msg):
     uuid, command = msg.data.split(MESSAGE_SEPARATOR)
-    print('received command {}'.format(command))
-    if 'yes' in command:
-        response_msg = MESSAGE_SEPARATOR.join([
-            uuid, 'understood, executing {}'.format(command), str(0)
-        ])
+    print("received command {}".format(command))
+    if "yes" in command:
+        response_msg = MESSAGE_SEPARATOR.join([uuid, "understood, executing {}".format(command), str(0)])
     else:
-        response_msg = MESSAGE_SEPARATOR.join([
-            uuid, "I'm not sure about {}, did you mean beer?".format(command), str(1)
-        ])
-    ROS_STATE['pub'].publish(response_msg)
-    print('replied {}'.format(response_msg))
+        response_msg = MESSAGE_SEPARATOR.join(
+            [uuid, "I'm not sure about {}, did you mean beer?".format(command), str(1)]
+        )
+    ROS_STATE["pub"].publish(response_msg)
+    print("replied {}".format(response_msg))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
